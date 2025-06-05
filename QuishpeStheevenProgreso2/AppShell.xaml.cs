4f34d21c3178eec051728209d242fb293979a69f
Stheeven Quishpe <stheeven.quishpe@udla.edu.ec>
2025-06-05 07:11:04 -0500

    Agregar archivos de proyecto.

---
+++ QuishpeStheevenProgreso2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace QuishpeStheevenProgreso2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
